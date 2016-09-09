f35eae23e468c89cde5f29a5bec45214c2c72a26
AleksanderBrzozowski <olek.brzozowski@gmail.com>
2016-09-09 16:24:18 +0200

    Create PlaysController. Add Spring boot jpa dependency. Create Play Jpa repository.

---
+++ src/main/java/com/example/controller/PlaysController.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by aleksander on 09.09.16.
+ */
+
+@RestController
+@RequestMapping("/plays")
+public class PlaysController {
+}
---
+++ src/main/java/com/example/repository/PlayRepository.java
@@ -0,0 +1,10 @@
+package com.example.repository;
+
+import com.example.entity.Play;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by aleksander on 09.09.16.
+ */
+public interface PlayRepository extends CrudRepository<Play, Integer> {
+}
