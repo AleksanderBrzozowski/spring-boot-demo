628f2921fad7beee1e33d3873f95060696642892
AleksanderBrzozowski <olek.brzozowski@gmail.com>
2016-09-11 12:58:16 +0200

    Refactor Application class to default.

--- src/main/java/com/example/DemoSpringBootApplication.java
+++ src/main/java/com/example/DemoSpringBootApplication.java
@@ -1,21 +1,12 @@
 package com.example;
 
-import com.example.repository.ActorRepository;
-import com.example.repository.PlayRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class DemoSpringBootApplication {
-
-    @Autowired
-    private ActorRepository actorRepository;
-    @Autowired
-    private PlayRepository playRepository;
-
+public class DemoSpringBootApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringBootApplication.class, args);
-
-	}
+    }
 }
