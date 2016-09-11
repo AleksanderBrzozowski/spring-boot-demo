86cb9181cb99d04f022f7af90cff1d1055995475
AleksanderBrzozowski <olek.brzozowski@gmail.com>
2016-09-11 16:13:26 +0200

    Change Actor repository to PagingAndSortingRepository. Change ActorController actors endpoint to use PagedResources.

--- src/main/java/com/example/controller/ActorController.java
+++ src/main/java/com/example/controller/ActorController.java
@@ -5,13 +5,13 @@ import com.example.entity.Actor;
 import com.example.repository.ActorRepository;
 import com.example.resource.ActorResource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.ExposesResourceFor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.hateoas.PagedResources;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * Created by Aleksander on 2016-09-10.
@@ -27,7 +27,8 @@ public class ActorController {
     ActorAssembler actorAssembler;
 
     @RequestMapping
-    public ResponseEntity<?> getActors() {
-        return new ResponseEntity<Object>(actorRepository.findAll().stream().map(actor -> actorAssembler.toResource(actor)).toArray(), HttpStatus.OK);
+    public PagedResources<ActorResource> actors(Pageable pageable, PagedResourcesAssembler<Actor> pagedAssembler) {
+        final Page<Actor> actors = actorRepository.findAll(pageable);
+        return pagedAssembler.toResource(actors, actorAssembler);
     }
 }
