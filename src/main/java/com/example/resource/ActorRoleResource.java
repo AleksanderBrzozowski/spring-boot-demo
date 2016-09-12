51fdcde3ada33a0922a49925472d0cb84bd335d2
AleksanderBrzozowski <olek.brzozowski@gmail.com>
2016-09-12 16:47:38 +0200

    Create ActorRoleResource.

---
+++ src/main/java/com/example/resource/ActorRoleResource.java
@@ -0,0 +1,20 @@
+package com.example.resource;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.hateoas.core.Relation;
+
+/**
+ * Created by aleksander on 12.09.16.
+ */
+
+@Getter
+@RequiredArgsConstructor
+@Relation(value = "role", collectionRelation = "filmography")
+public class ActorRoleResource extends ResourceSupport {
+
+    private final String playName;
+    private final String nameInFilm;
+    private final float stars;
+}
