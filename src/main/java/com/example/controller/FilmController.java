96c852e0ffc276e77c9e2cf1eb2e6ebd217765fa
AleksanderBrzozowski <olek.brzozowski@gmail.com>
2016-09-14 11:15:04 +0200

    Add method without implementation to get single film.

--- src/main/java/com/example/controller/FilmController.java
+++ src/main/java/com/example/controller/FilmController.java
@@ -2,7 +2,9 @@ package com.example.controller;
 
 import com.example.entity.Film;
 import com.example.repository.FilmRepository;
+import com.example.resource.FilmResource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,10 @@ public class FilmController {
     @RequestMapping
     public List<Film> getPlays() {
         return filmRepository.findAll();
+    }
+
+    @RequestMapping("/{id}")
+    public FilmResource film(@PathVariable int id) {
+        return null;
     }
 }
