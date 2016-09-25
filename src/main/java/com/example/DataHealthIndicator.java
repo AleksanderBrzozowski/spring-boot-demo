f6b0117ff0f0d3851e3c82f5cfa298c7867271b1
aleksander <olek.brzozowski@gmail.com>
2016-09-25 20:03:11 +0200

    Use lombok annotation constructor. Add other repositories count.

--- src/main/java/com/example/DataHealthIndicator.java
+++ src/main/java/com/example/DataHealthIndicator.java
@@ -1,8 +1,8 @@
 package com.example;
 
-import com.example.repository.ActorRepository;
-import com.example.repository.FilmRepository;
+import com.example.repository.*;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
@@ -13,22 +13,23 @@ import org.springframework.stereotype.Component;
  */
 
 @Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class DataHealthIndicator implements HealthIndicator {
 
     private @NonNull final ActorRepository actorRepository;
     private @NonNull final FilmRepository filmRepository;
-
-    @Autowired
-    public DataHealthIndicator(ActorRepository actorRepository, FilmRepository filmRepository) {
-        this.actorRepository = actorRepository;
-        this.filmRepository = filmRepository;
-    }
+    private @NonNull final ActorRoleRepository actorRoleRepository;
+    private @NonNull final PictureRepository pictureRepository;
+    private @NonNull final RatingRepository ratingRepository;
 
     @Override
     public Health health() {
         return Health.up()
                 .withDetail("Actors count", actorRepository.count())
                 .withDetail("Films count", filmRepository.count())
+                .withDetail("Actor roles count", actorRoleRepository.count())
+                .withDetail("Pictures count", pictureRepository.count())
+                .withDetail("Ratings count", ratingRepository.count())
                 .build();
     }
 }
